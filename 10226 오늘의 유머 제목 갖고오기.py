3b4897ed8fbedf20e082f6fb884a8ed5eda7d7df
uvwone <120762631+uvwone@users.noreply.github.com>
2022-12-30 23:03:15 +0900

    Update 10226 오늘의 유머 제목 갖고오기.py

--- 10226 오늘의 유머 제목 갖고오기.py
+++ 10226 오늘의 유머 제목 갖고오기.py
@@ -3,30 +3,37 @@
 import requests
 
 #headers없으면 네이버가 요청차단
-headers = \
-{'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'}
 
-url = 'https://news.naver.com/'                 #원하는 사이트 입력
-site = requests.get(url, headers=headers)       #내용 가져오기
-source_data = site.text                         #인터넷 소스코드를 source_data변수에 저장
+def get_url(url):
+    headers = \
+    {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'}
+    site = requests.get(url, headers=headers)       #내용 가져오기
+    source_data = site.text
+    return source_data
 
-count = source_data.count('"cjs_t">')           #뉴스 제목 개수 가져오기
 
-def a(pos1, pos2, extract_data)     #111111111111111111매개변수 살려줘
+def find_f(source_data, word):
+    pos = source_data.find(word) + len(word)
+    source_data = source_data[pos:]
+    return source_data
 
-      for i in range(count):                          #해당 개수만큼 반복
-            pos1 = source_data.find('"cjs_t">') + len('"cjs_t">')       #뉴스 앞 부분위치 지정
-            source_data = source_data[pos1:]                            #해당 위치로 이동
+def find_b(source_data, word):
+    pos = source_data.find(word)
+    extract_data = source_data[:pos]
+    return extract_data
 
-            pos2 = source_data.find('</div>')         #뉴스 뒷부분 위치 저장
-            extract_data = source_data[: pos2]        #앞부분부터 뒷부분까지 내용 추출해서 저장
+source_data = get_url('https://news.naver.com')
+count = source_data.count('"cjs_t">')
 
-            source_data = source_data[pos2+1:]         #다음뉴스를 찾기위해 뒷부분을 이동시키기
-            print(i+1, extract_data)                  #화면에 출력
-      return a  
-      
-      #def 함수이름(매개변수)
-            #코드블록
-            #return 함수이름
-            
-            #이거 실행 안된다
+
+for i in range(count):
+    source_data = find_f(source_data, '"cjs_t">')
+    extract_data = find_b(source_data, '</div>')
+            #pos1 = source_data.find('"cjs_t">') + len('"cjs_t">')       #뉴스 앞 부분위치 지정
+           # source_data = source_data[pos1:]                            #해당 위치로 이동
+
+            #pos2 = source_data.find('</div>')         #뉴스 뒷부분 위치 저장
+            #extract_data = source_data[: pos2]        #앞부분부터 뒷부분까지 내용 추출해서 저장
+
+            #source_data = source_data[pos2+1:]         #다음뉴스를 찾기위해 뒷부분을 이동시키기
+    print(i+1, extract_data)                  #화면에 출력
