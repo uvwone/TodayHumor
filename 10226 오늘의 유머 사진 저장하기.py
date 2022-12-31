6b63e8f76f158cc6de53067a301b31e7f9c20041
uvwone <120762631+uvwone@users.noreply.github.com>
2022-12-31 11:26:03 +0900

    Update 10226 오늘의 유머 사진 저장하기.py

--- 10226 오늘의 유머 사진 저장하기.py
+++ 10226 오늘의 유머 사진 저장하기.py
@@ -13,25 +13,25 @@ def get_url(url):
     return source_data
 
 def find_f(source_data, a):
-    pos = source_data.find(source_data, a) + len(source_data, a)
+    pos = source_data.find(a) + len(a)
     source_data = source_data[pos:]
     return source_data
 
 def find_b(source_data, a):
-    pos = source_data.find(source_data, a)
+    pos = source_data.find(a)
     extract_data = source_data[:pos]
     return extract_data
 
        
-      #sitelink
+#sitelink
+source_data = get_url('http://www.todayhumor.co.kr/board/list.php?table=bestofbest')
 count = source_data.count('<td class="subject">')
-source_data = get_url('http://www.todayhumor.co.kr/board/list.php?table=bestofbest&page={0}'.format(a))
 
 for i in range(count):
 
-    source_data = find_f(source_data, '<td class="subject">')
-    source_data = find_f(source_data, '<a href="')
-    extract_data = find_b(source_data, '" target="')
+      source_data = find_f(source_data, '<td class="subject">')
+      source_data = find_f(source_data, '<a href="')
+      sitelink = find_b(source_data, '" target="')
             #pos1 = source_data.find('<td class="subject">') + len('<td class="subject">')
             #source_data = source_data[pos1:]
 
@@ -41,11 +41,11 @@ for i in range(count):
             #pos3 = source_data.find('" target="')
             #sitelink = source_data[:pos3]
 
-      #title
+      #titleaa
 
-    source_data = find_f(source_data, 'top">')
-    extract_data = find_b(source_data, '</a>')
-    source_data = find_f(source_data, '<span')
+      source_data = find_f(source_data, 'top">')
+      title = find_b(source_data, '</a>')
+      source_data = find_f(source_data, '<span')
             
             #pos4 = source_data.find('top">') + len('top">')
             #source_data = source_data[pos4:]
@@ -59,23 +59,30 @@ for i in range(count):
             #source_data = source_data[pos6+1:]
 
       #image_range
-            #url2 = 'http://www.todayhumor.co.kr' + sitelink여기 안했음 여기 안했음 여기 안했음 여기 안했음 여기 안했음 여기 안했음 여기 안했음 여기 안했음
+      source_data = get_url('http://www.todayhumor.co.kr'+sitelink)
+      count2 = source_data.count('<img src="')
+            #url2 = 'http://www.todayhumor.co.kr' + sitelink
             #site = requests.get(url2, headers=headers)
             #source1_data = site.text
 
+      source_data = find_f(source_data, '<div class="viewContent">')
+      source_data = find_f(source_data, '</div><!--viewContent-->')
             #pos0 = source1_data.find('<div class="viewContent">') + len('<div class="viewContent">')
             #source1_data = source1_data[pos0:]
 
             #pos00 = source1_data.find('</div><!--viewContent-->')
             #source1_data = source1_data[:pos00]
 
-            count2 = source1_data.count('<img src="')
-            for j in range(count2):
-                  pos01 = source1_data.find('<img src="') + len('<img src="')
-                  source1_data = source1_data[pos01:]
 
-                  pos02 = source1_data.find('"')
-                  image = source1_data[:pos02]
+      for j in range(count2):
+
+            source_data = find_f(source_data, '<img src="')
+            image = find_b(source_data, '"')
+            #pos01 = source1_data.find('<img src="') + len('<img src="')
+            #source1_data = source1_data[pos01:]
+
+            #pos02 = source1_data.find('"')
+            #image = source1_data[:pos02]
 
                   #파일로 저장하기
 
@@ -86,7 +93,7 @@ for i in range(count):
                   ##file = open(file_name, 'wb')
                   #file.write(ss.content)
 
-                  file.close()
+                  ##file.close()
 
-                  print(j+1, '|', title, '||', image)
+            print(j+1, '|', title, '||', image)
             
